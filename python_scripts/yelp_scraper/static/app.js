57ad1ca1a1682ec0c481e7885e165de34daa0996
Ali Abdelmalek <Ali@Alis-MBP.fios-router.home>
2019-02-16 14:20:45 -0500

    Updated UI

--- python_scripts/yelp_scraper/static/app.js
+++ python_scripts/yelp_scraper/static/app.js
@@ -1,38 +1,38 @@
 var search_link;
 
-function generateLink(){
+function generateLink(event){
 
     var business_name = document.getElementById("business").value.trim()
     var location = document.getElementById("location").value.trim()
 
     search_link = "/scrape?search=" + business_name + "&location=" + location
 
-    // if(e.keyCode === 13){
-    //     getData(business_name,location);
-    // }
+    if(event.keyCode === 13){
+         getData(business_name,location);
+     }
 
 
 }
 
 function getData(){
-
+    console.log(search_link);
     var business_name = document.getElementById("business").value.trim()
     var location = document.getElementById("location").value.trim()
 
     if (business_name === "" && location === ""){
-        document.getElementById("business").style.cssText = "border-color: #ff4d4d; border-width:2px;"
-        document.getElementById("location").style.cssText = "border-color: #ff4d4d; border-width:2px;"
+        document.getElementById("business").style.cssText = "border-color: #FD0000; border-width:2px;"
+        document.getElementById("location").style.cssText = "border-color: #FD0000; border-width:2px;"
 
     }
 
     else if (business_name === "" && location !== ""){
-        document.getElementById("business").style.cssText = "border-color: #ff4d4d; border-width:2px;"
+        document.getElementById("business").style.cssText = "border-color: #FD0000; border-width:2px;"
         document.getElementById("location").style.cssText = ""
 
     }
 
     else if (business_name !== "" && location === ""){
-        document.getElementById("location").style.cssText = "border-color: #ff4d4d; border-width:2px;"
+        document.getElementById("location").style.cssText = "border-color: #FD0000; border-width:2px;"
         document.getElementById("business").style.cssText = ""
 
     }
@@ -42,7 +42,7 @@ function getData(){
         document.getElementById("business").style.cssText = ""
         document.getElementById("business").readOnly = true;
         document.getElementById("location").readOnly = true;
-        document.getElementById("scrapeSearch").readOnly = true;
+        document.getElementById("scrapeSearch").disabled = true;
         document.getElementById("progressBar").style.visibility = "visible";
         document.getElementById("finalResult").innerHTML = "";
 
@@ -57,7 +57,7 @@ function getData(){
             document.getElementById("finalResult").innerHTML = `The general sentiment for ${business_name} is ${sentiment}.`;
             document.getElementById("business").readOnly = false;
             document.getElementById("location").readOnly = false;
-            document.getElementById("scrapeSearch").readOnly = false;}
+            document.getElementById("scrapeSearch").disabled = false;}
 
             else{
                 console.log(data);
@@ -66,7 +66,7 @@ function getData(){
                 document.getElementById("finalResult").innerHTML = `There was an error retrieving the Yelp data. Please try again.`;
                 document.getElementById("business").readOnly = false;
                 document.getElementById("location").readOnly = false;
-                document.getElementById("scrapeSearch").readOnly = false;}
+                document.getElementById("scrapeSearch").disabled = false;}
 
             
 
