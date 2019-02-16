7449a239bad4f62e0bfa2fc4d791e8cc944d4195
Ali Abdelmalek <Ali@Alis-MBP.fios-router.home>
2019-02-16 18:30:25 -0500

    Fixed scraper, added loadbar, and made GUI nicer.

--- python_scripts/yelp_scraper/app.py
+++ python_scripts/yelp_scraper/app.py
@@ -8,6 +8,10 @@ import pandas as pd
 df = ""
 business_name = ""
 business_rating = ""
+business_page = ""
+business_page = ""
+percent_complete = 100
+complete = 0
 
 # Yelp Scraper Function
 def scraper_function(user_location,user_search):
@@ -15,6 +19,10 @@ def scraper_function(user_location,user_search):
         global df
         global business_name
         global business_rating
+        global business_page
+        global percent_complete
+        global complete
+
         # Get URL For Top Hit in Search
         search_url = "https://www.yelp.com/search?find_desc=" + user_search + "&find_loc=" + user_location
         print(f"search url: {search_url}")
@@ -43,10 +51,12 @@ def scraper_function(user_location,user_search):
         comments = []
         published_dates = []
         ratings = []
+        percent_complete = 10
 
         # Iterate Through Pages
         for i in range(3):
-            next_page = business_page + f"?start={i*20}"
+            percent_complete += 30
+            next_page = business_page.split("?")[0] + f"?start={i*20}"
             response = requests.get(next_page)
             business_soup = bs(response.text,"lxml")
             raw_comments = business_soup.find_all("p",itemprop="description")
@@ -61,23 +71,29 @@ def scraper_function(user_location,user_search):
             
             business_rating = raw_ratings[0].get("content")
             
-            raw_ratings = raw_ratings[1:len(raw_ratings)]
+            raw_ratings = raw_ratings = raw_ratings[1:21]
             
             [ratings.append(rating.get("content")) for rating in raw_ratings]
 
         print(f"business overall rating: {business_rating}")
 
         # Create Final DataFrame
+        print(len(published_dates))
+        print(len(comments))
+        print(len(ratings))
         df = pd.DataFrame({"Published Date":published_dates,
                         "Comment": comments,
                         "Rating" : ratings})
+        complete = 1
 
     except:
         df = pd.Series({"Published Date":"ERROR",
                         "Comment": "ERROR",
                         "Rating" : "ERROR"})
+
         business_name = "ERROR"
         business_rating = "ERROR"
+        complete = 1
 
 
 # Initialize Flask
@@ -89,10 +105,18 @@ def home():
 
     return render_template("index.html")
 
+@app.route("/percentComplete")
+def loading():
+    percent_series = pd.Series({"PERCENT_COMPLETE":percent_complete,
+                                "COMPLETE" : complete})
+    percent_json = percent_series.to_json(orient="index")
+
+    return percent_json
 
 @app.route("/scrape")
 def scrape():
-
+    global complete
+    complete = 0
     user_location = request.args["location"]
 
     user_search = request.args["search"]
@@ -104,10 +128,12 @@ def scrape():
 
     # Run df results through Model HERE
     sentiment = "positive"
-
+    complete = 1
     final_df = pd.Series({"BUSINESS_NAME" : business_name,
                              "BUSINESS_RATING":business_rating,
-                             "SENTIMENT":sentiment})
+                             "SENTIMENT":sentiment,
+                             "BUSINESS_URL":business_page,
+                             "COMPLETE": complete})
 
     final_json = final_df.to_json(orient="index")
 
--- python_scripts/yelp_scraper/static/app.js
+++ python_scripts/yelp_scraper/static/app.js
@@ -1,4 +1,5 @@
 var search_link;
+var complete;
 
 function generateLink(event){
 
@@ -10,12 +11,10 @@ function generateLink(event){
     if(event.keyCode === 13){
          getData(business_name,location);
      }
-
-
 }
 
 function getData(){
-    console.log(search_link);
+    // console.log(search_link);
     var business_name = document.getElementById("business").value.trim()
     var location = document.getElementById("location").value.trim()
 
@@ -45,22 +44,25 @@ function getData(){
         document.getElementById("scrapeSearch").disabled = true;
         document.getElementById("progressBar").style.visibility = "visible";
         document.getElementById("finalResult").innerHTML = "";
+        getPercent();
 
         d3.json(search_link).then(function(data){
+            
             var business_name = data["BUSINESS_NAME"];
             var sentiment = data["SENTIMENT"];
+            var business_page = data["BUSINESS_URL"];
 
             if (business_name != "ERROR"){
-            console.log(data);
+            // console.log(data);
             document.getElementById("progressBar").style.visibility = "hidden";
 
-            document.getElementById("finalResult").innerHTML = `The general sentiment for ${business_name} is ${sentiment}.`;
+            document.getElementById("finalResult").innerHTML = `The general sentiment for <a href='${business_page}' target='_blank'>${business_name}</a> is ${sentiment}.`;
             document.getElementById("business").readOnly = false;
             document.getElementById("location").readOnly = false;
             document.getElementById("scrapeSearch").disabled = false;}
 
             else{
-                console.log(data);
+                // console.log(data);
                 document.getElementById("progressBar").style.visibility = "hidden";
     
                 document.getElementById("finalResult").innerHTML = `There was an error retrieving the Yelp data. Please try again.`;
@@ -68,13 +70,35 @@ function getData(){
                 document.getElementById("location").readOnly = false;
                 document.getElementById("scrapeSearch").disabled = false;}
 
-            
-
-
     })}
 
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function getPercent(){
+    complete = 0;
+    for (var i = 0; i < 20; i++){
+        await sleep(2000);
+    d3.json("/percentComplete").then(function(data){
+
+        
+                var percent_complete = data["PERCENT_COMPLETE"];
+                complete = data["COMPLETE"];
+                // console.log(percent_complete);
+                // console.log(complete);
+                if (complete === 0){
+                document.getElementById("progressBar").style.cssText = `width : ${percent_complete}%; height:4px;`}
+    })
+    if (complete === 1){
+        // console.log(complete);
+        // console.log("breaking percent loop")
+        break;
+    }
+}
+}
 
 if (document.getElementById("progressBar") != null){
 
